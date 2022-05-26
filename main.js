5d850eddba6f296c3b1ac9e218b582de27b88161
-Kayk_Caputo- <49660516+KaykCaputo@users.noreply.github.com>
2022-05-26 13:08:06 -0300

    c

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ canvas.width=200;
 
 const ctx = canvas.getContext("2d");
 const road = new Road(canvas.width/2, canvas.height)
-const car = new Car(100,100,30,50);
+const car = new Car(road.getLaneCenter(3),100,30,50);
 
 animate();
 
--- road.js
+++ road.js
@@ -1,5 +1,5 @@
 class Road{
-    constructor(x,width,laneCount=4){
+    constructor(x,width,laneCount=3){
         this.x=x;
         this.width=width;
         this.laneCount=laneCount;
@@ -13,8 +13,9 @@ class Road{
     }
 
     getLaneCenter(laneIndex){
-        const lineWidth=this.width/this.laneCount;
-        return this.left+lineWidth/2+laneIndex*lineWidth;
+        const laneWidth=this.width/this.laneCount;
+        return this.left+laneWidth/2+
+        Math.min(laneIndex, this.laneCount-1)*laneWidth
     }
     draw(ctx){
         ctx.lineWidth=5;
