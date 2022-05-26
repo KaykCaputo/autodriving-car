8ff53a8ef2f087ef672ac9819992ee2fd4561b1c
-Kayk_Caputo- <49660516+KaykCaputo@users.noreply.github.com>
2022-05-26 12:52:43 -0300

    b

--- road.js
+++ road.js
@@ -1,5 +1,5 @@
 class Road{
-    constructor(x,width,laneCount=3){
+    constructor(x,width,laneCount=4){
         this.x=x;
         this.width=width;
         this.laneCount=laneCount;
@@ -15,10 +15,23 @@ class Road{
     draw(ctx){
         ctx.lineWidth=5;
         ctx.strokeStyle="white";
-
+        for(let i=0;i<this.laneCount;i++){
+            const x = lerp(
+                this.left,
+                this.right,
+                i/this.laneCount
+            );
+        }
+        if(i>0 && i<this.laneCount){
+            ctx.setLineDash([20,20]);
+        }else{
+            ctx.setLineDash([]);
+        }
         ctx.beginPath();
-        ctx.moveTo(this.right,this.top);
-        ctx.lineTo(this.left,this.botton);
+        ctx.moveTo(x,this.top);
+        ctx.lineTo(x,this.botton);
         ctx.stroke();
+
     }
 }
+
